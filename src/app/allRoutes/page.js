b971189a06eaba02baace0d969f3986fecf9fe75
marco <marcoramirezmail@gmail.com>
2024-12-23 18:08:11 -0800

    route collection page styling

--- src/app/allRoutes/page.js
+++ src/app/allRoutes/page.js
@@ -11,35 +11,45 @@ export default function AllRoutes() {
   console.log("form data: ", formData);
 
   if (!formData || formData.length === 0) {
-    return <p>No routes found. Please add some routes.</p>;
+    return (
+      <main className={styles.allRoutes}>
+        <Hero imageSrc="/img/img3.jpeg" />
+        <p className={styles.errorMessage}>
+          No routes found. Please add some routes.
+        </p>
+      </main>
+    );
   }
 
   return (
     <main className={styles.allRoutes}>
       <Hero imageSrc="/img/img3.jpeg" />
-      <h2>here's your routes!</h2>
-      <div className={styles.routes}>
-        {formData.map((route, index) => (
-          <div key={index} className={styles.route}>
-            <div className={styles.dataItem}>
-              <h3>Climb Name:</h3>
-              <p>{route.climbName}</p>
-              <Link href={`/routes/${route.slug}`}>View Details</Link>
+
+      <div className={styles.routesContainer}>
+        <h3>Your Routes:</h3>
+        <div className={styles.routes}>
+          {formData.map((route, index) => (
+            <div key={index} className={styles.route}>
+              <div className={styles.dataItem}>
+                <h3>Climb Name:</h3>
+                <p>{route.climbName}</p>
+                <Link href={`/routes/${route.slug}`}>View Details</Link>
+              </div>
+              <div className={styles.dataItem}>
+                <h3>Climb Type:</h3>
+                <p>{route.climbType}</p>
+              </div>
+              <div className={styles.dataItem}>
+                <h3>Sub Type:</h3>
+                <p>{route.subType}</p>
+              </div>
+              <div className={styles.dataItem}>
+                <h3>Notes:</h3>
+                <p>{route.notes}</p>
+              </div>
             </div>
-            <div className={styles.dataItem}>
-              <h3>Climb Type:</h3>
-              <p>{route.climbType}</p>
-            </div>
-            <div className={styles.dataItem}>
-              <h3>Sub Type:</h3>
-              <p>{route.subType}</p>
-            </div>
-            <div className={styles.dataItem}>
-              <h3>Notes:</h3>
-              <p>{route.notes}</p>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </main>
   );
